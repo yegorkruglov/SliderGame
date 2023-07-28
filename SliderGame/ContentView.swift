72fd7c9f33fe0b7f9a3d233b71ae50f45c84d022
Egor Kruglov <yegorkruglov@gmail.com>
2023-07-28 14:12:55 +0300

    Initializer setup of content view

--- SliderGame/ContentView.swift
+++ SliderGame/ContentView.swift
@@ -8,12 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var targetValue = Int.random(in: 0...100)
+    @State private var sliderValue = Double.random(in: 0...100).rounded()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            Text("Подвинь слайдер, как можно ближе к: \(targetValue)")
+            
+            HStack {
+                Text("0")
+                Slider(value: $sliderValue, in: 0...100, step: 1)
+                Text("100")
+            }
+            
+            Button("Проверь меня") {
+                
+            }
+            
+            Button("Начать заново") {
+                
+            }
+            
         }
         .padding()
     }
---
+++ SliderGame/UISliderRepresentation.swift
@@ -0,0 +1,26 @@
+//
+//  UISliderRepresentation.swift
+//  SliderGame
+//
+//  Created by Egor Kruglov on 28.07.2023.
+//
+
+import SwiftUI
+
+struct UISliderRepresentation: UIViewRepresentable {
+    func makeUIView(context: Context) -> UISlider {
+        <#code#>
+    }
+    
+    func updateUIView(_ uiView: UISlider, context: Context) {
+        <#code#>
+    }
+        
+    
+}
+
+struct UISliderRepresentation_Previews: PreviewProvider {
+    static var previews: some View {
+        UISliderRepresentation()
+    }
+}
