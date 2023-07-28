925a0f2c4087acba7daba1479420646c9e28fb7d
Egor Kruglov <yegorkruglov@gmail.com>
2023-07-28 13:58:34 +0300

    Initial Commit

---
+++ SliderGame/SliderGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameApp.swift
+//  SliderGame
+//
+//  Created by Egor Kruglov on 28.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
